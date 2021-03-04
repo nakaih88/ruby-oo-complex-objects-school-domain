0a07d6e06e20a1d1808f8ab7269916ca96aabc75
Nakai <nakai.hamlin18@gmail.com>
2021-03-03 23:56:32 -0500

    commit tree 7913ce110d742507159e1447226a05c31b2ea270

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+attr_accessor :roster, :student_name, :school_name
+
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << student_name
+    end
+
+    def grade(grade)
+        @roster.select { |grades, student_hash| return student_hash if grades == grade}
+    end
+
+    def sort
+        sorted_hash = {}
+        self.roster.sort.each do |array|
+            sorted_hash[array[0]] = array[1].sort
+        end
+        sorted_hash
+    end
+end
